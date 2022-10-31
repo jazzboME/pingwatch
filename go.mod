7572d32b7d3283721e37c84ed1faea8e4b0339d4
Kevin Foss <kevin.foss@gmail.com>
2022-10-30 21:48:17 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/jazzboME/pingwatch
+
+go 1.19
+
+require (
+	github.com/prometheus-community/pro-bing v0.1.0
+	github.com/schollz/progressbar/v3 v3.11.0
+)
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20220615171555-694bf12d69de // indirect
+	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+)
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	probing "github.com/prometheus-community/pro-bing"
+	"github.com/schollz/progressbar/v3"
+)
+
+type pktStat struct {
+	seq  int
+	time time.Time
+}
+
+var curPinging bool
+var lastRecv, lastSend pktStat
+var curPingRun int
+var bar *progressbar.ProgressBar
+
+func main() {
+	silent := flag.Bool("s", false, "")
+	flag.Parse()
+
+	host := flag.Arg(0)
+	pinger, err := probing.NewPinger(host)
+	pinger.SetPrivileged(true)
+	pinger.Debug = true
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	lastRecv.seq = -2
+	lastRecv.time = time.Now()
+
+	bar = progressbar.NewOptions(-1,
+		progressbar.OptionSetDescription("pingwatch"),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSpinnerType(7),
+		progressbar.OptionSetItsString("ping"),
+	)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			pinger.Stop()
+		}
+	}()
+
+	pinger.OnRecv = func(pkt *probing.Packet) {
+		curPinging = true
+		if lastRecv.seq < pkt.Seq-1 && lastRecv.seq >= 0 {
+			curTime := time.Now()
+			duration := curTime.Sub(lastRecv.time)
+			curPingRun = 0
+			if !(*silent) {
+				fmt.Printf("%s: Restoring after ping break from %d to %d (%s).\n",
+					prettyTime(curTime), lastRecv.seq, pkt.Seq, duration)
+				bar.Set(0)
+				bar.Reset()
+			}
+			log.Printf("%s: Restoring after ping break from %d to %d (%s).\n",
+				prettyTime(curTime), lastRecv.seq, pkt.Seq, duration)
+		}
+		if lastRecv.seq > pkt.Seq {
+			curPingRun = 0
+			if !(*silent) {
+				fmt.Printf("\n%s: Received out of sequence ping, %d after %d.\n",
+					prettyTime(time.Now()), pkt.Seq, lastRecv.seq)
+				bar.Set(0)
+				bar.Reset()
+			}
+			log.Printf("%s: Received out of sequence ping, %d after %d.\n",
+				prettyTime(time.Now()), pkt.Seq, lastRecv.seq)
+		}
+		if lastRecv.seq == pkt.Seq-1 {
+			curPingRun++
+			if !(*silent) {
+				bar.Add(1)
+			}
+		}
+		lastRecv = pktStat{seq: pkt.Seq, time: time.Now()}
+	}
+
+	pinger.OnSend = func(pkt *probing.Packet) {
+		curTime := time.Now()
+		if (pkt.Seq > lastRecv.seq+1) && curPinging {
+			if !(*silent) {
+				fmt.Printf("\n%s: Sending but not receiving.\n", prettyTime(curTime))
+			}
+			log.Printf("%s: Sending but not receiving.\n", prettyTime(curTime))
+			curPinging = false
+		}
+		lastSend = pktStat{seq: pkt.Seq, time: curTime}
+	}
+
+	pinger.OnFinish = func(stats *probing.Statistics) {
+		if !(*silent) {
+			fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
+			fmt.Printf("%d packets transmitted, %d packets received, %d duplicates, %v%% packet loss\n",
+				stats.PacketsSent, stats.PacketsRecv, stats.PacketsRecvDuplicates, stats.PacketLoss)
+			fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+				stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
+		}
+
+		log.Printf("\n--- %s ping statistics ---\n", stats.Addr)
+		log.Printf("%d packets transmitted, %d packets received, %d duplicates, %v%% packet loss\n",
+			stats.PacketsSent, stats.PacketsRecv, stats.PacketsRecvDuplicates, stats.PacketLoss)
+		log.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+			stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
+	}
+
+	if !(*silent) {
+		fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
+	}
+	log.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
+	err = pinger.Run()
+	if err != nil {
+		fmt.Println("Failed to ping target host:", err)
+	}
+}
+
+func prettyTime(t time.Time) string {
+	return t.Format("Mon Jan 2, 2006 03:04:05.9 pm")
+}
